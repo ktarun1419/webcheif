eb05d4e63d9d5d0aff6b522e937a35360602183e
ktarun1419 <86646398+ktarun1419@users.noreply.github.com>
2022-04-17 16:49:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# webcheif
-PECFEST-LHSK7
+# design1
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,563 @@
+<template>
+  <div id="app">
+    <div>
+      <nav id="menu">
+        <input
+          type="checkbox"
+          id="responsive-menu"
+          onclick="updatemenu()"
+        /><label></label>
+        <ul>
+          <li><a>LOGIN</a></li>
+          <li><a>BUY NOW</a></li>
+          <li><a>STORE</a></li>
+          <li><a>GUIDE</a></li>
+          <li><a>EQUIP</a></li>
+          <li><a>COMMUNIOTY</a></li>
+          <li><a>NEWS</a></li>
+          <li><a>EXPLORE</a></li>
+          <!-- <li><a class='dropdown-arrow' >Ethereum</a>
+      <ul class='sub-menus'>
+        <li><a>Ethereum</a></li>
+        <li><a>Ethereum</li>
+      </ul>
+    </li> -->
+          <li><img src="./assets/GalaxyWars.png" class="logowars" alt="" /></li>
+        </ul>
+      </nav>
+    </div>
+    <div>
+       <div class="div1"><img src="./assets/r1.png" alt="" width="100%"/> </div>
+    <div class="div2"><img src="./assets/r2.png" alt="" width="100%" height="230px"/></div>
+    <div class="div3"><img src="./assets/r3.png" alt=""  width="100%" height="230px"></div>
+    </div>
+    <br>
+    <div >
+      <div class="div4">
+        <img src="./assets/Rectangle 30.png" alt="" class="img1"/>
+        <h4 style="color:grey">1 APR 2022</h4>
+        <h3 >Update II Live Now</h3>
+         <img src="./assets/Rectangle 36.png" alt="" class="img1"/>
+          <h4 style="color:grey">1 APR 2022</h4>
+        <h3 >PC Alpha Phase 4: Features, Fixes and Known Issues</h3>
+        </div>
+      <div class="div5">
+        <img src="./assets/Rectangle 31.png" alt="" class="img1"/>
+         <h4 style="color:grey">1 APR 2022</h4>
+        <h3 >Odyssey Console Development Update</h3>
+        <img src="./assets/Rectangle 37.png" alt="" class="img1"/>
+         <h4 style="color:grey">1 APR 2022</h4>
+        <h3 >Prepare to disembark, Commander. Your Odyssey begins on PC, 19 May</h3>
+        </div>
+      <div class="div6">
+        <img src="./assets/Rectangle 32.png" alt="" class="img1"/>
+         <h4 style="color:grey">1 APR 2022</h4>
+        <h3 >Odyssey Update 9 Now Live </h3>
+         <img src="./assets/Rectangle 38.png" alt="" class="img1"/>
+          <h4 style="color:grey">1 APR 2022</h4>
+        <h3 >Phase 3: Features, Fixes, and Known Issues</h3>
+        </div>
+     
+    </div>
+    <div> <img src="./assets/image 5.png" width="100%" alt="" style="margin-top:100px"></div>
+    <div><img src="./assets/grad.png" width="100%" alt="" style="margin-top:200px"> <h2 class="text1">REPORT ANOMALIES</h2></div>
+    <div>
+      <span class="inp"><input class="inp1" type="name" placeholder="Name"><img src="./assets/person-circle 1.png"  alt=""></span>
+    </div>
+    <div>
+      <span class="inp2"><input class="inp3" type="email" placeholder="Email"><img src="./assets/envelope 1.png"  alt=""></span>
+    </div>
+    <div>
+      <span class="inp4"><input class="inp5" type="text" placeholder="Write your comment here...."><img src="./assets/journal-text 1.png"  alt=""></span>
+    </div>
+    <button class="btn">POST</button>
+    <footer class="footer1">
+      <div><div class="divf4"><img src="./assets/GalaxyWars.png" class="logowars" width="200px" alt="" /></div>
+    <div class="divf5">
+      <ul class="ful"><li> GALAXY WARS
+        </li>
+        <li> ABOUT US 
+        </li> 
+         <li> STORE 
+        </li>
+         <li> LOGIN 
+        </li></ul>
+        <ul class="ful"><li> <img src="./assets/Vector.png" alt="">
+        </li>
+        <li> <img src="./assets/Twitter.png" alt="">
+        </li> 
+         <li> <img src="./assets/Instagram.png" alt="">
+        </li>
+         <li> <img src="./assets/Youtube.png" alt=""> 
+        </li><li><img src="./assets/Vector (3).png" alt=""></li></ul><br>
+        <ul class="ful"><li> <img src="./assets/PS4.png" alt="">
+        </li>
+        <li> <img src="./assets/Steam.png" alt="">
+        </li> 
+         <li> <img src="./assets/X-box.png" alt="">
+        </li>
+         <li> <img src="./assets/EpicGames.png" alt=""> 
+        </li></ul><br>
+         <h3>  PEC ACM CSS © 2022. All logos and features are registered under Fastest Frontend First team’s jurisdiction.
+Copying and usage of logos, assets and other information of “GALAXY WARS”are to be done with permission only. 
+Anyways, if you reached till here, then you are a winner in yourself. Kudos !</h3>
+    </div>
+    <div class="divf6"><img src="./assets/ACM2.png" class="logowars" width="180px" alt="" /></div></div>
+      <div>
+        <div class="divf4"><img src="./assets/ACM.png" class="logowars" width="200px" alt="" /></div>
+    <div class="divf52">
+      <ul class="ful">
+        <li> ABOUT US 
+        </li> 
+         <li> OUR GAMES
+        </li>
+         <li> CAREERS
+        </li><li> NEWS
+        </li><li> INVESTORS
+        </li><li> CONTACT
+        </li> <li> <img src="./assets/Cobra.png" alt="">
+        </li> </ul>
+        <ul class="ful">
+        <li> PRIVACY POLICY & LEGAL
+        </li> 
+         <li> DO NOT SELL MY PERSONAL INFORMATION 
+        </li>
+         <li> TERMS AND CONDITIONS
+        </li><li> STORE
+        </li><li> FORUMS</li>
+        </ul>
+         <ul class="ful"><li> <img src="./assets/Vector.png" alt="">
+        </li>
+        <li> <img src="./assets/Twitter.png" alt="">
+        </li> 
+         <li> <img src="./assets/Instagram.png" alt="">
+        </li>
+         <li> <img src="./assets/Youtube.png" alt=""> 
+        </li><li><img src="./assets/Vector (3).png" alt=""></li></ul>
+        <h5>© 2022 PEC ACM CSS Student Chapter. All Rights Reserved</h5>
+    </div>
+        
+        
+    </div>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: "Inter var", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color:white;
+  font-weight: 600;
+ position: relative;
+width: 100%;
+height: 3367px;
+
+background: url(./assets/space_bg.jpeg);
+}.divf4{
+  text-align: left;
+  margin-top: 60px;
+  width: 20%;
+  float: left;
+  
+}
+.divf5{
+  text-align: left;
+  align-content: left;
+  margin-top: 50px;
+  width: 53%;
+  display: inline-block;
+}
+.divf52{
+  text-align: left;
+  align-content: left;
+  margin-top: 50px;
+  width: 70%;
+  display: inline-block;
+}
+.divf6{
+  text-align: left;
+  margin-top: 50px;
+
+  width: 20%;
+ float: right;
+}.footer1{
+  width: 100%;
+  margin-top:50px
+}.ful{
+  color:white;
+  float: left;
+}.ful li{
+  display: inline;
+  position: relative;
+  margin:10px
+}
+.btn{
+  
+  font-family: "Inter var", sans-serif;
+  color:black;
+  font-size: 15px;
+  background: #f4a123;
+  padding: 10px 15px 10px 15px;
+  text-decoration: none;
+  width: 140px;
+  
+  cursor: pointer;
+  margin-top: 60px;
+}
+
+.btn:hover {
+  background: #ffffff;
+  text-decoration: none;
+  color: white;
+  font-weight: 600;
+  background-color: black;
+  opacity: 0.8;
+  border: 3px solid #f4a123;
+}.inp1{
+
+  padding: 8px;
+  width: 200px;
+  border:0px;
+  
+}
+.inp{
+  margin-top: 100px;
+  margin-bottom: 40px;
+  background-color: white;
+  width: 250px;
+  height: 40px;
+  padding: 10px;
+  border-radius: 20px;
+  float: left;
+  margin-left: 20%;
+}
+.inp5{
+
+  padding: 8px;
+  width: 950px;
+  border:0px;
+  
+  
+}
+.inp4{
+  padding-top: 100px;
+  background-color: white;
+  width: 1030px;
+  height: 200px;
+  padding: 20px;
+  border-radius: 20px;
+  margin-left:12%
+  
+}
+.inp3{
+
+  padding: 8px;
+  width: 600px;
+  border:0px;
+  
+}
+.inp2{
+  margin-top: 100px;
+  background-color: white;
+  width: 700px;
+  height: 40px;
+  padding: 10px;
+  border-radius: 20px;
+  float: right;
+  margin-right: 100px;
+}
+.text1{
+  z-index: 2;
+  display: block;
+  position: fixed;
+}.img1{
+  width: 90%;
+  border: yellow solid 1px;
+}
+.img1:hover{
+ border:red solid 1.5px;
+ cursor: pointer;
+width: 100%;
+}
+.f1{
+  z-index: 4;
+  padding: 20px 10px ;
+}.div4{
+  text-align: left;
+  margin-top: 60px;
+  width: 28%;
+  float: left;
+  margin-left: 2%;
+}
+.div5{
+  text-align: left;
+  margin-top: 50px;
+  width: 28%;
+  display: inline-block;
+}
+.div6{
+  text-align: left;
+  margin-top: 50px;
+  margin-right: 2%;
+  width: 28%;
+ float: right;
+}
+.div1 {
+  width: 50%;
+  float: left;
+}
+.div2 {
+  width: 50%;
+  float: right;
+}
+.div3 {
+  width: 50%;
+  float: right;
+  margin-top: -6px;
+}
+.logowars {
+  float: left;
+  margin-top: 5px;
+}
+
+#menu {
+  background-color: black;
+  color: white;
+  height: 80px;
+  padding-left: 18px;
+
+  border-radius: 0;
+  border-bottom: 1px solid #000000;
+  z-index: 7;
+}
+#menu ul,
+#menu li {
+  margin: 0 auto;
+  padding: 0;
+  list-style: none;
+  z-index: 7;
+}
+#menu ul {
+  width: 100%;
+  z-index: 7;
+  background-color: black;
+}
+#menu li {
+  display: inline;
+  position: relative;
+  margin-top: 0px;
+  z-index: 7;
+}
+#menu a {
+  margin-right: 20px;
+  float: right;
+  cursor: pointer;
+  display: block;
+  line-height: 43px;
+  padding: 10px 10px;
+  text-decoration: none;
+  font-weight: 600;
+  color: white;
+  font-size: 15px;
+  margin-top: 10px;
+  text-transform: capitalize;
+  font-family: "Inter var", sans-serif;
+  z-index: 7;
+}
+#menu a.dropdown-arrow:after {
+  content: "\25BE";
+  margin-left: 5px;
+  z-index: 7;
+}
+#menu li a:hover {
+  color: #000000;
+  font-weight: 600;
+  z-index: 7;
+  background-color: #f4a123;
+}
+#menu input {
+  display: none;
+  margin: 0;
+  padding: 0;
+  height: 45px;
+  width: 100%;
+  opacity: 0;
+  cursor: pointer;
+  z-index: 7;
+}
+#menu label {
+  display: none;
+  line-height: 43px;
+  text-align: center;
+  position: absolute;
+  left: 35px;
+  z-index: 7;
+}
+#menu label:before {
+  font-size: 1.6em;
+  content: "\2261";
+  margin-left: 20px;
+  z-index: 7;
+}
+#menu ul.sub-menus {
+  height: auto;
+  overflow: hidden;
+  width: 170px;
+  background: transparent;
+  position: absolute;
+  z-index: 99;
+  display: none;
+  z-index: 7;
+}
+#menu ul.sub-menus li {
+  display: block;
+  width: 100%;
+  z-index: 7;
+}
+#menu ul.sub-menus a {
+  color: #575757;
+  font-size: 16px;
+  z-index: 7;
+}
+#menu li:hover ul.sub-menus {
+  display: block;
+}
+#menu ul.sub-menus a:hover {
+  background: #ffffff;
+  color: #000000;
+}
+@media screen and (max-width: 800px) {
+  #menu {
+    position: relative;
+  }
+  #menu ul {
+    background: #111;
+    position: absolute;
+    top: 100%;
+    right: 0;
+    left: 0;
+    z-index: 3;
+    height: auto;
+    display: none;
+  }
+  #menu ul.sub-menus {
+    width: 100%;
+    position: static;
+  }
+  #menu ul.sub-menus a {
+    padding-left: 30px;
+  }
+  #menu li {
+    display: block;
+    float: none;
+    width: auto;
+  }
+  #menu input,
+  #menu label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+  }
+  #menu input {
+    z-index: 4;
+  }
+  #menu input:checked + label {
+    color: white;
+  }
+  #menu input:checked + label:before {
+    content: "\00d7";
+  }
+  #menu input:checked ~ ul {
+    display: block;
+  }
+}
+.modal1 {
+  display: block;
+  position: fixed; /* Stay in place */
+  //z-index: 4;
+  left: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+}
+
+/* Modal Content/Box */
+.modal-content1 {
+  border: 1px solid rgb(151, 147, 147);
+  background-color:transparent; /* Black w/ opacity */
+  color: white;
+z-index: 4;
+  padding: 20px;
+  
+  width: 100%; /* Could be more or less, depending on screen size */
+}
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: normal;
+  align-items: stretch;
+  align-content: stretch;
+}
+
+.flex-items:nth-child(1) {
+  display: block;
+  flex-grow: 0;
+  flex-shrink: 1;
+  flex-basis: auto;
+  align-self: auto;
+  order: 0;
+  border: 1px solid rgb(94, 90, 90);
+  border-radius: 30px;
+  margin: 20px;
+}
+
+.flex-items:nth-child(2) {
+  display: block;
+  flex-grow: 0;
+  flex-shrink: 1;
+  flex-basis: auto;
+  align-self: auto;
+  order: 0;
+  border: 1px solid rgb(94, 90, 90);
+  border-radius: 30px;
+  margin: 20px;
+}
+@media screen and (max-width: 400px) {
+  .div1{
+    width: 100%;
+    margin-top: 10px;
+  }.div3{
+    width: 100%;
+    margin-top: 10px;
+  }.div2{
+    width: 100%;
+    margin-top: 10px;
+  }.div4{
+    width: 100%;
+    margin: 0;
+  }.div5{
+    width: 100%;
+     margin: 0;
+  }.div6{
+    width: 100%;
+     margin: 0;
+  }.inp{
+    margin: 0;
+  }.inp1{
+    margin: 0;
+  }.inp2{
+    margin: 0;
+  }.inp3{
+    margin: 0;
+  }.inp4{
+    margin: 0;
+  }.inp5{
+    margin: 0;
+  }
+}
+</style>
